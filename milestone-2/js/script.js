5252dd288f25908f2b79d2c54006e2e2a375489e
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-17 21:49:44 +0200

    sul console log mi si vedono le immagini ma nell'esercizio non si vedono

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -9,21 +9,60 @@ const images = [
   `01.jpg`, `02.jpg`, `03.jpg`, `04.jpg`, `05.jpg`
 ];
 
-let imageTags = ``;
 
 //contenitore
-const contImg = document.querySelector(`.objects-wrapper`);
+const objectWrapper = document.querySelector(`.objects-wrapper`);
+
+let imageTags = ``;
 //ciclare array
 
 for(let i = 0; i < images.length; i++){
   imageTags += `
-  <img class="object" src="img/${images[i]}" alt="${images[i]}">
+  <img class="object" src="img/${images[i]}" alt="${images[i]}" class="object">
   `;
 }
 
-let counterImages = 0
+console.log(objectWrapper);
 
-//aggiunta tag nel cont-img
-contImg.innerHTML += imageTags;
+let counterImages = 0;
 
+
+// bottoni
+const next = document.querySelector(`.bottom`);
+const previous = document.querySelector(`.top`);
+previous.classList.add("hide")
+
+//image tags
+
+objectWrapper.innerHTML += imageTags;
+
+const objects = document.getElementsByClassName(`object`)
+
+objects[counterImages].classList.add(`active`);
+
+//click cambiamento img
+
+next.addEventListener("click" , function(){
+  objects[counterImages].classList.remove(`active`);
+  objects[++counterImages].classList.add(`active`);
+
+  previous.classList.remove(`hide`);
+  if(counterImages === images.lenght -1){
+    next.classList.add(`hide`)
+  }
+
+});
+
+previous.addEventListener("click" , function(){
+  objects[counterImages].classList.remove(`active`);
+  objects[--counterImages].classList.add(`active`);
+
+  next.classList.remove(`hide`);
+  if(counterImages === images.lenght -1){
+    next.classList.add(`hide`)
+  }
+
+});
+
+console.log(objects);
 
