e08ee2ee70dbbf101424bfcb01d6865ea23bd909
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-18 10:36:29 +0200

    correzione

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -9,22 +9,22 @@ const images = [
   `01.jpg`, `02.jpg`, `03.jpg`, `04.jpg`, `05.jpg`
 ];
 
+let counterImages = 0;
+let imageTags = ``;
 
 //contenitore
 const objectWrapper = document.querySelector(`.objects-wrapper`);
 
-let imageTags = ``;
 //ciclare array
 
 for(let i = 0; i < images.length; i++){
   imageTags += `
-  <img class="object" src="img/${images[i]}" alt="${images[i]}" class="object">
+  <img class="object" src="img/${images[i]}" alt="${images[i]}">
   `;
 }
 
 console.log(objectWrapper);
 
-let counterImages = 0;
 
 
 // bottoni
